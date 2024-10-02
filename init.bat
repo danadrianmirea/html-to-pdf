1e0ccddc9212f0d2a5b43a484938c28507b2c507
Dan Adrian Mirea <damirea84@gmail.com>
2024-10-03 00:06:13 +0300

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# html-to-pdf" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/html-to-pdf.git
-git push -u origin main
